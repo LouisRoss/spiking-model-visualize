239e2dd89581ce12eebf8826eb06552f49456e5d
Louis Ross <louis.ross@gmail.com>
2021-08-01 18:17:47 -0700

    Working detailed visualization

---
+++ tests/connectionTest.js
@@ -0,0 +1,41 @@
+
+class ConnectionTest {
+    constructor(brain) {
+        this.brain = brain;
+
+        this.frameCount = 0;
+        this.showingConnections = false;
+        this.showingConnectionsIndexes = [];
+
+        this.initializeTest();
+    }
+
+    initializeTest() {
+    }
+
+    doTest() {
+      this.frameCount--;
+      if (this.frameCount <= 0) {
+        if (this.showingConnections) {
+          for (const indexToShow of this.showingConnectionsIndexes) {
+            this.brain.hideAllConnections(indexToShow);
+          }
+          this.showingConnectionsIndexes = [];
+          this.showingConnections = false;
+          this.frameCount = 2;
+        }
+        else {
+          for (var i = 0; i < 5; i++) {
+            const indexToShow = Math.floor(Math.random() * 1000);
+            this.showingConnectionsIndexes.push(indexToShow);
+            this.brain.showAllConnections(indexToShow);
+          }
+          this.showingConnections = true;
+          this.frameCount = 5;
+        }
+
+      }
+    }
+}
+
+export { ConnectionTest };
